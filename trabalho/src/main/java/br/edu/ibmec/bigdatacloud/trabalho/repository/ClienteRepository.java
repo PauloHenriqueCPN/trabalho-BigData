3fa5730f7ff06a349148b7f8b0e858d6814e59af
unknown <phnunes2004@gmail.com>
2024-10-01 04:22:26 -0300

    esqueci o @Repository

--- trabalho/src/main/java/br/edu/ibmec/bigdatacloud/trabalho/repository/ClienteRepository.java
+++ trabalho/src/main/java/br/edu/ibmec/bigdatacloud/trabalho/repository/ClienteRepository.java
@@ -2,9 +2,12 @@ package br.edu.ibmec.bigdatacloud.trabalho.repository;
 
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
 import br.edu.ibmec.bigdatacloud.trabalho.model.Cliente;
 import jakarta.validation.Valid;
 
+@Repository
 public class ClienteRepository {
 
     public Cliente save(@Valid Cliente cliente) {
